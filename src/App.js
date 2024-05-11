71ea551b14203989d543fd0352173676e40b4d75
Abhijith cb <abhijithcb706@gmail.com>
2024-05-11 22:45:14 +0530

    added email input for contacts

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Resources from "./components/Resources";
+import NewsLetter from "./components/NewsLetter";
 function App() {
   return (
     <div>
     <Navbar/>
     <Hero/>
     <Resources/>
+    <NewsLetter/>
     </div>
     
   );
