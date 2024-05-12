74773525bb685ee9e9bb1090130c13b1db5a7594
Abhijith cb <abhijithcb706@gmail.com>
2024-05-12 22:33:18 +0530

    premium cards added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Resources from "./components/Resources";
 import NewsLetter from "./components/NewsLetter";
+import Premium from "./components/Premium";
 function App() {
-  return (
-    <div>
-    <Navbar/>
-    <Hero/>
-    <Resources/>
-    <NewsLetter/>
-    </div>
-    
-  );
+return (
+<div>
+  <Navbar />
+  <Hero />
+  <Resources />
+  <NewsLetter />
+  <Premium />
+</div>
+
+);
 }
 
 export default App;
